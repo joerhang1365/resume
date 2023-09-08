5cd3ad43e53027986c030fc6952a00b370562b54
Joseph Hanger <48699911+joerhang1365@users.noreply.github.com>
2023-09-07 18:25:17 -0900

    Add files via upload

--- resume.c
+++ resume.c
@@ -11,34 +11,37 @@ struct job
 {
   char title[32];
   char company_name[32];
-  unsigned int year;
+  uint32_t year;
   char description[512];
 };
 
 int main(int argc, char **argv)
 {
-  /* experience and skills */
-  int32_t blender_modeling, lathe_machining, vertical_mill_machining = 43;
-  char mig_welding, stick_welding, wood_working, video_editing, web_design = '\0';
-  bool CPR = true;
+  /* eduction */
+  printf(
+      "University Of Illinois Urbana-Champaign || Computer Engineering || Freshman\n"
+      "Southwestern Illinois College || Associates of Science and Associates of"
+      "Arts May 2023 || GPA: 3.8\n"
+      "Red Bud High School || High School Diploma May 2023 || GPA: 3.9\n"
+      );
 
   /* previous jobs */
   struct job job1 = 
   {
-    .title = "counselor/tutor",
+    .title = "Counselor/Tutor",
     .company_name = "AmeriCorps",
     .year = 2023,
     .description = 
       "Volunteered at the school Ellis Elementry in Belleville, IL. Was tasked"
       "with creating and executing activities, arts, crafts, and lessons for"
-      "fifth grade students that relate to specific subjects like STEM."
+      "fifth grade students that relate to specific subjects such as STEM."
       "Volunteered at multiple other sites such as the Shelter Shop and TOCO"
       "Shop in Belleville, IL."
   };
 
   struct job job2 =
   {
-    .title = "manufacturing internship",
+    .title = "Manufacturing Internship",
     .company_name = "Roeslein & Associates, Inc.",
     .year = 2022,
     .description =
@@ -51,7 +54,7 @@ int main(int argc, char **argv)
 
   struct job job3 = 
   {
-    .title = "lifeguard",
+    .title = "Lifeguard",
     .company_name = "Red Bud YMCA",
     .year = 2021,
     .description =
@@ -61,27 +64,25 @@ int main(int argc, char **argv)
       "for Basic Life Support, Basic First Aid, and Emergency Oxygen."
   };
 
-  /* eduction */
-  printf(
-      "Red Bud High School: High School Diploma May 2023\n"
-      "Southwestern Illinois College: Associates of Science and Associates of"
-      "Arts May 2023\n"
-      "University Of Illinois Urbana-Champaign: Freshman\n"
-      );
-
   /* experience and skills */
-  int32_t national_honors_society, cross_country, track, waterloo_municipal_band = 101;
-  int16_t game_development, future_buisness_leaders_of_america, academi_challenge = 2;
-  int8_t rock_climbig, illinois_robotics_in_space = 64;
+  int16_t blender_modeling, lathe_machining, vertical_mill_machining,
+          mig_welding, stick_welding, wood_working, video_editing, web_design = 255;
+  bool CPR = true;
+
+ 
+  /* hobbies and clubs */
+  int8_t national_honors_society, cross_country, track, waterloo_municipal_band,
+         game_development, future_buisness_leaders_of_america, academi_challenge,
+         rock_climbig, illinois_robotics_in_space = 127;
 
   /* projects */
-  char computer_2023[512] = 
+  char W650C2S_2023[512] = 
     "Created a 8-bit computer using a W65C02S microprocessor and bread boards."
     "This project was inspired by a video series created by Ben Eater"
     "(youtube series link: https://www.youtube.com/watch?v=LnzuMJLZRdU). In the"
     "end I created a functional 8-bit computer that could be programmed using an"
     "EEPROM and 6502 assembly. Right now the computer can output text to an"
-    "analog display. Later I hope to design a PCB for the computer and create a"
+    "analog display. I am currently designing a PCB for the computer will create a"
     "basic operating system.";
 
   char national_honors_society_website_2022[512] =
@@ -92,7 +93,7 @@ int main(int argc, char **argv)
     "While making this project I learned about receiving and sending data using"
     "HTTPS and Java logic for web user input.";
 
-  // most of my programming projects are on my Github.
+  // check out more of my projects on github.
 
   return 0;
 }
